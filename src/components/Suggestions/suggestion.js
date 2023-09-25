dbde1c226f60d7800fb481c8c584471d3f4904e8
Jerry Martinez <jaysavageg@gmail.com>
2023-09-25 14:41:49 -0400

    one small step forward

--- src/components/Suggestions/suggestion.js
+++ src/components/Suggestions/suggestion.js
@@ -6,7 +6,19 @@ export default function Suggestions () {
             <div>
                 <img style={styles} id='layed' src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2e9bbfc9-bc2d-42c3-8156-6d2289adf46d/dfwhsn4-ae227cf2-e9e0-467a-b68b-dfa8b1b7b85c.png/v1/fill/w_1194,h_669,q_70,strp/nezuko_kamado_by_volcorei_dfwhsn4-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzE4IiwicGF0aCI6IlwvZlwvMmU5YmJmYzktYmMyZC00MmMzLTgxNTYtNmQyMjg5YWRmNDZkXC9kZndoc240LWFlMjI3Y2YyLWU5ZTAtNDY3YS1iNjhiLWRmYThiMWI3Yjg1Yy5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.cPye3thWguCsIy8yggZ5cGeSpHC42EtOALRa7FAlBqQ' alt='demonSlayer'/>
                 <h2>Volcorei - Nezuko Kamado</h2>
-                <p>Check out more of this artist work here: <a href='https://www.deviantart.com/volcorei/gallery'>Volcorei</a></p>
+                <p style={styles} id='artistLink'>Check out more of this artist work here: <a href='https://www.deviantart.com/volcorei/gallery'>Volcorei</a></p>
+
+                <br/>
+
+                <img style={styles} id='upRight' src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/23c346e9-0ca1-4c4d-8448-3227102041cd/deqshks-02a9e59d-98c5-42d9-8e17-312796a6a678.png/v1/fit/w_828,h_1216,q_70,strp/hatsune_miku_by_p0iy0_deqshks-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU0MyIsInBhdGgiOiJcL2ZcLzIzYzM0NmU5LTBjYTEtNGM0ZC04NDQ4LTMyMjcxMDIwNDFjZFwvZGVxc2hrcy0wMmE5ZTU5ZC05OGM1LTQyZDktOGUxNy0zMTI3OTZhNmE2NzgucG5nIiwid2lkdGgiOiI8PTEwNTAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.6MvGbzkfdjCXP0vbwF8Rqb1PCt0G4v4afKp89NwmS2s' alt='miku'/>
+                <h2>p0iy0 - Hatsune Miku</h2>
+                <p style={styles} id='artistLink'>Check out more of this artist work here: <a href='https://www.deviantart.com/p0iy0/gallery'>p0iy0</a></p>
+
+                <br/>
+
+                <img style={styles} id='layed' src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/08a71aa5-8a92-45b0-9a4e-5144d7910f0c/d8jibqv-e5b64b57-25e1-48ef-9044-86d7003c0c01.jpg/v1/fill/w_1169,h_684,q_70,strp/tokyo_ghoul___kaneki_by_tinani_d8jibqv-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODc4IiwicGF0aCI6IlwvZlwvMDhhNzFhYTUtOGE5Mi00NWIwLTlhNGUtNTE0NGQ3OTEwZjBjXC9kOGppYnF2LWU1YjY0YjU3LTI1ZTEtNDhlZi05MDQ0LTg2ZDcwMDNjMGMwMS5qcGciLCJ3aWR0aCI6Ijw9MTUwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.5zd-acwppyucbZrXvkKgkpWZ98LkiUnXkXa2EezHT7o' alt='tokyoGhoul'/>
+                <h2>Tokyo Ghoul - Kaneki</h2>
+                <p style={styles} id='artistLink'>Check out more this artist work here: <a href='https://www.deviantart.com/tinani/gallery'>Tinani</a></p>
             </div>
     )
 }
